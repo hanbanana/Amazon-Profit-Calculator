3f7bad68ff6ea55ffbb918af66a81e70cbc5204e
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-22 14:54:51 -0700

    add files

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+ $(document).ready(function() {
+
+
+ function initializeCalculator() {
+    firstNumber = "";
+    secondNumber = "";
+    operator = "";
+    isOperatorChosen = false;
+    isCalculated = false;
+
+    $("#first-number, #second-number, #operator, #result").empty();
+  }
+
+
+
+
+  $(".equal").on("click", function() {
+
+  })
+
+
+
+
+
+
+})
